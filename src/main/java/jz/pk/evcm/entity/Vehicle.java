402d6700690aab7eacfbf73af0cdb7df882b71b6
jzielinski47 <jzielinski47dev@gmail.com>
2026-06-12 18:37:01 +0200

    added vehicle entity

---
+++ src/main/java/jz/pk/evcm/entity/Vehicle.java
@@ -0,0 +1,21 @@
+package jz.pk.evcm.entity;
+
+import jakarta.persistence.*;
+
+import java.time.Year;
+
+@Entity
+public class Vehicle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User owner;
+
+    private String brand;
+    private String model;
+    private Year yearOfProduction;
+
+}
