a03230c8ef1429aa1402cbdc56978d9dc2e3a231
jzielinski47 <jzielinski47dev@gmail.com>
2026-06-24 16:56:15 +0200

    implemented RestClient directly inside the constructor for OCM Service

--- src/main/java/jz/pk/evcm/service/OCMService.java
+++ src/main/java/jz/pk/evcm/service/OCMService.java
@@ -1,8 +1,13 @@
 package jz.pk.evcm.service;
 
+import jz.pk.evcm.dto.req.ocm.ChargerPointDto;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
+
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 /*
 * OPEN CHARGE MAP SERVICE to call the API for the list of chargers
@@ -11,20 +16,25 @@ import org.springframework.web.client.RestClient;
 
 @Service
 public class OCMService {
-    private final RestClient restClient;
+    RestClient restClient;
     private final String apiBaseUrl = "https://api.openchargemap.io/v3";
-    private final String apiKey;
 
-    public OCMService(
-            RestClient.Builder restClientBuilder,
-            @Value("${OPEN_CHARGE_API_KEY}") String apiKey) {
-
-        this.apiKey = apiKey;
-        this.restClient = restClientBuilder
+    public OCMService(@Value("${OPEN_CHARGE_API_KEY}") String apiKey) {
+        this.restClient = RestClient.builder()
                 .baseUrl(apiBaseUrl)
-                .defaultHeader("X-API-Key", this.apiKey)
+                .defaultHeader("X-API-Key", apiKey)
+                .defaultHeader("Accept", "application/json")
                 .build();
     }
 
     // TODO: Implement OpenChargeAPI requests
+    public List<ChargerPointDto> getAllChargerPointsAround(Double latitude, Long longitude) {
+
+        return new ArrayList<>();
+    }
+
+    public List<ChargerPointDto> ocmTestEndpoint() {
+        // call https://api.openchargemap.io/v3/poi/?output=json&countrycode=US&maxresults=10
+       return new ArrayList<>();
+    }
 }
