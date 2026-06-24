429594778173200aff4c61b9fb85bc1e786de6ea
jzielinski47 <jzielinski47dev@gmail.com>
2026-06-24 16:18:45 +0200

    implemented OpenChargeAPI integration service (base endpoint configuration) for further calls (missing from previous commits due to my mistake)

--- src/main/java/jz/pk/evcm/config/AppConfig.java
+++ src/main/java/jz/pk/evcm/config/AppConfig.java
@@ -1,4 +1,15 @@
 package jz.pk.evcm.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+@Configuration
 public class AppConfig {
+
+    @Bean
+    public RestClient.Builder restClientBuilder() {
+        return RestClient.builder();
+    }
+
 }
--- src/main/java/jz/pk/evcm/service/OCMService.java
+++ src/main/java/jz/pk/evcm/service/OCMService.java
@@ -1,4 +1,30 @@
 package jz.pk.evcm.service;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+/*
+* OPEN CHARGE MAP SERVICE to call the API for the list of chargers
+* docs: https://www.openchargemap.org/develop/api
+*/
+
+@Service
 public class OCMService {
+    private final RestClient restClient;
+    private final String apiBaseUrl = "https://api.openchargemap.io/v3";
+    private final String apiKey;
+
+    public OCMService(
+            RestClient.Builder restClientBuilder,
+            @Value("${OPEN_CHARGE_API_KEY}") String apiKey) {
+
+        this.apiKey = apiKey;
+        this.restClient = restClientBuilder
+                .baseUrl(apiBaseUrl)
+                .defaultHeader("X-API-Key", this.apiKey)
+                .build();
+    }
+
+    // TODO: Implement OpenChargeAPI requests
 }
