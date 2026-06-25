36648fa8421c969961ff02d76840a051c363fc9d
jzielinski47 <jzielinski47dev@gmail.com>
2026-06-25 10:13:32 +0200

    updated fetch endpoint for OpenChargeAPI

--- src/main/java/jz/pk/evcm/controller/ChargerPointControlller.java
+++ src/main/java/jz/pk/evcm/controller/ChargerPointControlller.java
@@ -1,11 +1,14 @@
 package jz.pk.evcm.controller;
 
+import jz.pk.evcm.dto.req.ocm.ChargerPointDto;
 import jz.pk.evcm.service.OpenChargeApiService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/chargers")
@@ -17,10 +20,10 @@ public class ChargerPointControlller {
         this.openChargerAPI = openChargerAPI;
     }
 
-    @GetMapping("/ocm")
-    public ResponseEntity<String> test1() {
-        String req = String.valueOf(openChargerAPI.fetchAllChargersInProximity(50.0,20.0,10.));
-        System.out.println(req);
+    @GetMapping("/poi")
+    public ResponseEntity<List<ChargerPointDto>> fetchAllChargersInProximity() {
+        List<ChargerPointDto> req = openChargerAPI.fetchAllChargersInProximity(50.0,20.0,10.);
+        System.out.println("Fetched " + req.size() + " chargers from OpenCharge API");
         return new ResponseEntity<>(req, HttpStatus.FOUND);
 
     }
