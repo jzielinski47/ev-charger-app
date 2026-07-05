5744c2aa4cb848ac7ce8be9de4dba724b0afeadd
jzielinski47 <jzielinski47dev@gmail.com>
2026-07-05 18:51:20 +0200

    configure filter chain with JWT validation, disable csrf checkup, add cors policy ruleset

--- src/main/java/jz/pk/evcm/config/WebSecurityConfig.java
+++ src/main/java/jz/pk/evcm/config/WebSecurityConfig.java
@@ -1,28 +1,61 @@
 package jz.pk.evcm.config;
 
+import jz.pk.evcm.security.JwtAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 class WebSecurityConfig {
+    private final AuthenticationProvider authProvider;
+    private final JwtAuthFilter jwtAuthFilter;
+
+    public WebSecurityConfig(AuthenticationProvider jwtAuthProvider, JwtAuthFilter jwtAuthFilter) {
+        this.authProvider = jwtAuthProvider;
+        this.jwtAuthFilter = jwtAuthFilter;
+    }
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) {
-        // @formatter:off
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/home").permitAll()
-                        .anyRequest().permitAll()
-                );
-        // @formatter:on
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authenticationProvider(authProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
 
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
 
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedMethods(List.of("GET", "POST"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
 }
