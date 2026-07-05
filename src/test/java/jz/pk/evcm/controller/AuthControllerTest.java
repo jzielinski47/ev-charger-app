c2c943b68dbb136a8a8bbfbce0955b7ed67ad3d8
jzielinski47 <jzielinski47dev@gmail.com>
2026-07-05 20:32:53 +0200

    add test for authcontroller

---
+++ src/test/java/jz/pk/evcm/controller/AuthControllerTest.java
@@ -0,0 +1,15 @@
+package jz.pk.evcm.controller;
+
+import jz.pk.evcm.config.AppConfig;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.context.WebApplicationContext;
+
+public class AuthControllerTest {
+
+    // TODO: test authentication endpoints
+
+}
