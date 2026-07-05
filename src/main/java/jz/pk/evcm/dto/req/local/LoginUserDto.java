cee64df01c3a56b61637d992e717c22ce3cbd128
jzielinski47 <jzielinski47dev@gmail.com>
2026-07-05 19:44:00 +0200

    convert authentications response and request dtos to records

--- src/main/java/jz/pk/evcm/dto/req/local/LoginUserDto.java
+++ src/main/java/jz/pk/evcm/dto/req/local/LoginUserDto.java
@@ -2,15 +2,12 @@ package jz.pk.evcm.dto.req.local;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Data;
 
-@Data
-public class LoginUserDto {
+public record LoginUserDto(
+        @NotBlank(message = "Email is required")
+        @Email(message = "Please provide a valid email address")
+        String email,
 
-    @NotBlank(message = "Email is required")
-    @Email(message = "Please provide a valid email address")
-    private String email;
-
-    @NotBlank(message = "Password is required")
-    private String password;
-}
+        @NotBlank(message = "Password is required")
+        String password
+) {}
---
+++ src/main/java/jz/pk/evcm/dto/res/LoginResponse.java
@@ -0,0 +1,3 @@
+package jz.pk.evcm.dto.res;
+
+public record LoginResponse(String token, long expiresIn) {}
