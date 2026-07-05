ed71043f5b49c81085492857992de96ff93c37d1
jzielinski47 <jzielinski47dev@gmail.com>
2026-07-05 13:39:37 +0200

    [WIP] made first skeleton for Jwt service class with common fields.

--- src/main/java/jz/pk/evcm/service/JwtService.java
+++ src/main/java/jz/pk/evcm/service/JwtService.java
@@ -1,22 +1,64 @@
+/*
+* following service was implemented with docs ref. https://github.com/jwtk/jjwt
+* */
+
+
 package jz.pk.evcm.service;
 
 import io.jsonwebtoken.Claims;
-import jz.pk.evcm.entity.UserRole;
+import io.jsonwebtoken.Jwts;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
+
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
-
 @Service
 public class JwtService {
-    @Value("${security.jwt.secret")
-    private String secret;
-
-    @Value("${security.jwt.expiration}")
-    private Long jwtExpiration;
+    @Value("${security.jwt.secret-key}")
+    private String secretKey;
 
 
 
+    @Value("${security.jwt.expiration-time}")
+    private long jwtExpirationMs;
 
+   public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+       final Claims claims = extractAllClaims(token);
+       return claimsResolver.apply(claims);
+   }
+
+   public String generateToken(String email) {
+       return Jwts.builder()
+               .subject(email)
+               .issuedAt(new Date())
+               .expiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
+               .signWith(getSignInKey())
+               .compact();
+   }
+
+    public String extractEmail(String token) {
+        return null;
+    }
+
+    public String extractRole(String token) {
+        return null;
+    }
+
+    public Long getExpirationTime(String token) {
+        return null;
+    }
+
+   private Claims extractAllClaims(String token) {
+
+   }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 }
