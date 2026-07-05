14a2e566cd863889ccc6fa9d2029fc820f9d42a7
jzielinski47 <jzielinski47dev@gmail.com>
2026-07-05 14:09:00 +0200

    [WIP] added parser methods for JwtService

--- src/main/java/jz/pk/evcm/service/JwtService.java
+++ src/main/java/jz/pk/evcm/service/JwtService.java
@@ -1,21 +1,25 @@
 /*
-* following service was implemented with docs ref. https://github.com/jwtk/jjwt
-* */
+ * following service was implemented with docs ref. https://github.com/jwtk/jjwt
+ * */
 
 
 package jz.pk.evcm.service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-
-import java.security.Key;
-import java.util.Date;
-import java.util.function.Function;
-
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import jz.pk.evcm.entity.UserRole;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Service
 public class JwtService {
@@ -23,41 +27,67 @@ public class JwtService {
     private String secretKey;
 
 
-
     @Value("${security.jwt.expiration-time}")
     private long jwtExpirationMs;
 
-   public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
-       final Claims claims = extractAllClaims(token);
-       return claimsResolver.apply(claims);
-   }
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
 
-   public String generateToken(String email) {
-       return Jwts.builder()
-               .subject(email)
-               .issuedAt(new Date())
-               .expiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
-               .signWith(getSignInKey())
-               .compact();
-   }
+    public String generateToken(String email, Set<UserRole> roles) {
+        return Jwts.builder()
+                .claim("role", wrapAuthorites(roles))
+                .subject(email)
+                .claims()
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
+                .and().signWith(getSigningKey())
+                .compact();
+    }
 
     public String extractEmail(String token) {
         return null;
     }
 
-    public String extractRole(String token) {
-        return null;
+    @SuppressWarnings("unchecked")
+    public List<UserRole> extractRoles(String token) {
+        List<String> rolesList = extractClaim(token, claims -> claims.get("roles", List.class));
+        return rolesList.stream()
+                .map(UserRole::valueOf)
+                .collect(Collectors.toList());
     }
 
-    public Long getExpirationTime(String token) {
-        return null;
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
     }
 
-   private Claims extractAllClaims(String token) {
+    public Long getExpirationTime() {
+        return jwtExpirationMs;
+    }
 
-   }
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String email = extractEmail(token);
+        return (email.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
 
-    private Key getSignInKey() {
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private List<String> wrapAuthorites(Set<UserRole> roles) {
+        return roles.stream().map(Enum::name).collect(Collectors.toList());
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private SecretKey getSigningKey() {
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
     }
