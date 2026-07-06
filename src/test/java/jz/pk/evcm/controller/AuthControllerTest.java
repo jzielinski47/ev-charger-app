b742902af429ca21a961cc64a67112be01d69bce
jzielinski47 <jzielinski47dev@gmail.com>
2026-07-06 11:41:57 +0200

    organize imports

--- src/test/java/jz/pk/evcm/controller/AuthControllerTest.java
+++ src/test/java/jz/pk/evcm/controller/AuthControllerTest.java
@@ -1,13 +1,8 @@
 package jz.pk.evcm.controller;
 
-import jz.pk.evcm.config.AppConfig;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.web.context.WebApplicationContext;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 public class AuthControllerTest {
 
     // TODO: test authentication endpoints
