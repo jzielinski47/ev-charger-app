c0320761822416421c1b254e29b21eebda4f3e4b
jzielinski47 <jzielinski47dev@gmail.com>
2026-07-06 14:39:14 +0200

    reduce number of endpoints for better maintainability at this project stage

--- src/main/java/jz/pk/evcm/controller/VehicleController.java
+++ src/main/java/jz/pk/evcm/controller/VehicleController.java
@@ -1,7 +1,6 @@
 package jz.pk.evcm.controller;
 
-import jz.pk.evcm.dto.req.local.InputVehicleDto;
-import jz.pk.evcm.dto.res.ResVehicleDto;
+import jz.pk.evcm.dto.res.VehicleResponse;
 import jz.pk.evcm.service.VehicleService;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -25,35 +24,10 @@ public class VehicleController {
     }
 
     @GetMapping
-    public List<ResVehicleDto> getVehicles(Authentication auth, @RequestParam(required = false) String targetEmail) {
+    public List<VehicleResponse> getVehicles(Authentication auth, @RequestParam(required = false) String targetEmail) {
         return vehicleService.getAllVehicles(auth.getName(), isAdmin(auth), targetEmail);
     }
 
-    @GetMapping("/{id}")
-    public ResVehicleDto getVehicleById(Authentication auth, @PathVariable Long id) {
-        return vehicleService.getVehicleById(id, auth.getName(), isAdmin(auth));
-    }
 
-    @PostMapping
-    public ResVehicleDto addVehicle(Authentication auth, @RequestBody InputVehicleDto dto) {
-        return vehicleService.addVehicle(dto, auth.getName(), isAdmin(auth));
-    }
 
-    @PutMapping("/{id}")
-    public ResVehicleDto modifyVehicle(
-            Authentication auth,
-            @PathVariable Long id,
-            @RequestBody InputVehicleDto dto) {
-        return vehicleService.modifyVehicle(id, dto, auth.getName(), isAdmin(auth));
-    }
-
-    @DeleteMapping("/{id}")
-    public void removeVehicle(Authentication auth, @PathVariable Long id) {
-        vehicleService.removeVehicle(id, auth.getName(), isAdmin(auth));
-    }
-
-    @PostMapping("/{id}/favourite")
-    public boolean setFavouriteVehicle(Authentication auth, @PathVariable Long id) {
-        return vehicleService.setFavouriteVehicle(id, auth.getName(), isAdmin(auth));
-    }
 }
