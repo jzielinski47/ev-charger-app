5610c47c7fac7463fb5b58c769e3858782129a66
jzielinski47 <jzielinski47dev@gmail.com>
2026-07-06 14:44:39 +0200

    add hibernate method for resolving vehicle by id and the owner email

--- src/main/java/jz/pk/evcm/repository/VehicleRepository.java
+++ src/main/java/jz/pk/evcm/repository/VehicleRepository.java
@@ -3,7 +3,9 @@ package jz.pk.evcm.repository;
 import jz.pk.evcm.entity.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+import java.util.Optional;
 
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
     List<Vehicle> findByOwnerEmail(String email);
+    Optional<Vehicle> findByIdAndOwnerEmail(Long id, String email);
 }
