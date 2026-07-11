e56fab567459989ef890e65d02c0543fbf1ab7f1
jzielinski47 <jzielinski47dev@gmail.com>
2026-07-11 12:53:56 +0200

    remove boilerplate response dtos

--- src/main/java/jz/pk/evcm/dto/res/ConnectionTypeResDto.java
+++
@@ -1,8 +0,0 @@
-package jz.pk.evcm.dto.res;
-
-public record ConnectionTypeResDto(
-        Long id,
-        String title,
-        String formalName
-) {
-}
--- src/main/java/jz/pk/evcm/dto/res/MinimalChargerPointDto.java
+++
@@ -1,8 +0,0 @@
-package jz.pk.evcm.dto.res;
-
-public record MinimalChargerPointDto(
-        Long id,
-        Double latitude,
-        Double longitude
-) {
-}
