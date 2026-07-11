149b16527fb9ad080180e5c4fb4b85275dc76c25
jzielinski47 <jzielinski47dev@gmail.com>
2026-07-11 14:12:21 +0200

    add default administrator user account

--- src/main/java/jz/pk/evcm/config/AppConfig.java
+++ src/main/java/jz/pk/evcm/config/AppConfig.java
@@ -1,6 +1,10 @@
 package jz.pk.evcm.config;
 
+import jz.pk.evcm.entity.User;
+import jz.pk.evcm.entity.UserRole;
 import jz.pk.evcm.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -43,5 +47,30 @@ public class AppConfig {
         authProvider.setPasswordEncoder(passwordEncoder());
 
         return authProvider;
+    }
+
+    @Bean
+    public CommandLineRunner seedDefaultUsers(@Value("${admin.email}") String adminEmail,
+                                              @Value("${admin.password}") String adminPassword) {
+        return new CommandLineRunner() {
+            @Override
+            public void run(String... args) throws Exception {
+                System.out.println("Instantiating default users during server startup...");
+
+                if (userRepository.findByEmail(adminEmail).isEmpty()) {
+
+                     User admin = new User();
+                     admin.setEmail(adminEmail);
+                     admin.setPassword(passwordEncoder().encode(adminPassword));
+                     admin.assignRole(UserRole.USER);
+                     admin.assignRole(UserRole.ADMIN);
+
+                    System.out.println("Admin user created: " + adminEmail);
+                } else {
+                    System.out.println("Admin user already exists.");
+                }
+
+            }
+        };
     }
 }
