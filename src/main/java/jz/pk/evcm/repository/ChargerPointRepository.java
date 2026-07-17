69cfba45c8a325a68742ab855d66cac809a3b0b1
jzielinski47 <jzielinski47dev@gmail.com>
2026-07-17 13:18:37 +0200

    update query with left join for addressInfo and connections which fixes the hibernate lazy load problem

--- src/main/java/jz/pk/evcm/repository/ChargerPointRepository.java
+++ src/main/java/jz/pk/evcm/repository/ChargerPointRepository.java
@@ -9,6 +9,9 @@ import java.util.List;
 
 public interface ChargerPointRepository extends JpaRepository<ChargerPoint, Long> {
 
-    @Query("SELECT cp FROM ChargerPoint cp WHERE function('ST_DWithin', cp.addressInfo.location, :userLocation, :distance) = true")
+    @Query("SELECT cp FROM ChargerPoint cp " +
+            "LEFT JOIN FETCH cp.connections " +
+            "LEFT JOIN FETCH cp.addressInfo " +
+            "WHERE function('ST_DWithin', cp.addressInfo.location, :userLocation, :distance) = true")
     List<ChargerPoint> findNearbyChargers(@Param("userLocation") Point userLocation, @Param("distance") double distance);
 }
