cbb2cd4d8431ee57c8fd8bec569faa5b04b94e60
jzielinski47 <jzielinski47dev@gmail.com>
2026-07-17 13:20:09 +0200

    enabled method security for  future graphql endpoint RBAC

--- src/main/java/jz/pk/evcm/config/WebSecurityConfig.java
+++ src/main/java/jz/pk/evcm/config/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -19,6 +20,7 @@ import java.util.List;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 class WebSecurityConfig {
     private final AuthenticationProvider authProvider;
     private final JwtAuthFilter jwtAuthFilter;
