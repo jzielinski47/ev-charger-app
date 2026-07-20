77548fa9564cbefc0d3494bc8877731afc31e292
jzielinski47 <jzielinski47dev@gmail.com>
2026-07-20 14:57:48 +0200

    implement skeleton for vehicle service layer tests

--- src/test/java/jz/pk/evcm/service/VehicleServiceTest.java
+++ src/test/java/jz/pk/evcm/service/VehicleServiceTest.java
@@ -1,5 +1,14 @@
 package jz.pk.evcm.service;
 
+import jz.pk.evcm.repository.VehicleRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class VehicleServiceTest {
+
+    @Mock
+    private VehicleRepository vehicleRepository;
 
 }
