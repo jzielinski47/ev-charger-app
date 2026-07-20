15977251d4372a816a2c27e6d68540d2c661c231
jzielinski47 <jzielinski47dev@gmail.com>
2026-07-20 15:41:38 +0200

    cleanup of longish constructors

--- src/main/java/jz/pk/evcm/service/VehicleService.java
+++ src/main/java/jz/pk/evcm/service/VehicleService.java
@@ -8,20 +8,17 @@ import jz.pk.evcm.entity.User;
 import jz.pk.evcm.entity.Vehicle;
 import jz.pk.evcm.repository.UserRepository;
 import jz.pk.evcm.repository.VehicleRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class VehicleService {
 
     private final VehicleRepository vehicleRepository;
     private final UserRepository userRepository;
-
-    public VehicleService(VehicleRepository vehicleRepository, UserRepository userRepository) {
-        this.vehicleRepository = vehicleRepository;
-        this.userRepository = userRepository;
-    }
 
     public List<VehicleResponse> getAllVehicles(String userEmail, boolean isAdmin, String targetUserEmail) {
         List<Vehicle> vehicles;
