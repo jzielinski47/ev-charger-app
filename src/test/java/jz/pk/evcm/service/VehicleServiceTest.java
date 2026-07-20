7e118c27d0c98809830b02a258f87b0f7ba7bb93
jzielinski47 <jzielinski47dev@gmail.com>
2026-07-20 15:03:30 +0200

    create skeleton methods for further implementation of unit tests with mockito

--- src/test/java/jz/pk/evcm/service/VehicleServiceTest.java
+++ src/test/java/jz/pk/evcm/service/VehicleServiceTest.java
@@ -1,7 +1,9 @@
 package jz.pk.evcm.service;
 
 import jz.pk.evcm.repository.VehicleRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -10,5 +12,35 @@ public class VehicleServiceTest {
 
     @Mock
     private VehicleRepository vehicleRepository;
+
+    @InjectMocks
+    private VehicleService vehicleService;
+
+    @Test
+    public void VehicleService_GetAllVehiclesAsAdmin_ReturnsAllVehicles() {
+
+    }
+
+    @Test
+    public void VehicleService_GetAllVehiclesAsUser_ReturnsAllVehicles() {
+
+    }
+
+    @Test
+    public void VehicleService_GetAllYourVehiclesAsUser_ReturnsAllYourVehicles() {
+
+    }
+
+    @Test
+    public void VehicleService_GetAllSingleUsersVehiclesAsAdmin_ReturnsAllSingleUsersVehicles() {
+
+    }
+
+    @Test
+    public void VehicleService_GetAllSingleUsersVehiclesAsOtherUser_ReturnsException() {
+
+    }
+
+
 
 }
